40f3ef452a2da9ceb7a321503c8fa7f43a91c812
altinawiwaseem <wasemm@live.com>
2023-05-01 16:42:39 +0200

    adding a session array

--- index.php
+++ index.php
@@ -1,9 +1,42 @@
 <?php
+// start session
+session_start();
+
 require_once('php/CreateDb.php');
 require_once('./php/component.php');
 
 // create instance of Createdb class
 $database = new CreateDb(dbname:"Productdb", tablename:"Producttb");
+
+if(isset($_POST["add"])){
+  //  print_r($_POST["product_id"]);
+  if(isset($_SESSION["cart"])){
+
+  $item_array_id =  array_column($_SESSION["cart"],"product_id" );
+
+  if(in_array($_POST["product_id"], $item_array_id)){
+echo"<script> alert('Product is already added in the cart')</script>";
+echo"<script> window.location='index.php' </script>";
+  }else{
+$count = count($_SESSION["cart"]);
+$item_array = array(
+    "product_id"=>$_POST["product_id"]
+);
+$_SESSION["cart"][$count] = $item_array;
+print_r($_SESSION["cart"]);
+  }
+
+  }else{
+    $item_array = array(
+        "product_id"=>$_POST["product_id"]
+    );
+
+    //create a new session variable
+    $_SESSION["cart"][0] = $item_array;
+    print_r($_SESSION["cart"]);
+
+  }
+}
 
 ?>
 
@@ -29,7 +62,7 @@ $database = new CreateDb(dbname:"Productdb", tablename:"Producttb");
        <?php
       $result= $database->getData();
      while($row = mysqli_fetch_assoc($result)) {
-         component($row["product_name"],$row["product_price"],$row["product_image"], $row["id"],);
+         component($row["product_name"],$row["product_price"],$row["product_image"], $row["id"]);
       }
        ?>
     </div>
